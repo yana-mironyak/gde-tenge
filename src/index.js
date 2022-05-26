7b8398ef8803d473c17a400bcd6270cca62b77fa
Dmytro Chushko <dmytro.chushko@gmail.com>
2022-05-27 00:27:42 +0300

    add transition and hidden effects

--- src/index.js
+++ src/index.js
@@ -1,22 +1,35 @@
-sttButton = document.querySelector('.stt-button');
+import throttle from 'lodash.throttle';
 
-window.addEventListener('scroll', onScroll);
+const sttButton = document.querySelector('.stt-button');
+let timerId = null;
 
-// window.onscroll = () => {
-//   if (window.scrollY > 700) {
-//     sttButton.classList.remove('.stt-button--hidden');
-//   } else if (window.scrollY < 700) {
-//     sttButton.classList.add('.stt-button--hidden');
-//   }
-// }
+window.onscroll = throttle(onScroll, 500);
+sttButton.onclick = onClick;
+
+function hideButton() {
+  sttButton.classList.add('stt-button--hidden'); 
+}
 
 function onScroll() {
-  
+  if (timerId) {
+    clearTimeout(timerId);
+  }
+   
   if (window.scrollY > 700) {
-    console.log('scrolling');
     sttButton.classList.remove('stt-button--hidden');
   }
   else if (window.scrollY > 600 && window.scrollY < 700) {
-    sttButton.classList.add('stt-button--hidden');
+    hideButton();
   }
+
+  timerId = setTimeout(hideButton, 1000);
+}
+
+function onClick() {
+  console.log('click');
+  window.scrollTo({
+  top: 0,
+  left: 0,
+  behavior: 'smooth'
+});
 }
